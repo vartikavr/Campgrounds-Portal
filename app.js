0cd805c0b4b1ee515a93aa34ae18f24230bc5208
Vartika Rana <vartikarana2000@gmail.com>
2021-02-11 16:57:58 +0530

    add start script

--- app.js
+++ app.js
@@ -171,6 +171,8 @@ app.use((err,req,res,next)=>{
     res.status(status).render('error',{err});
 })
 
-app.listen(3000,()=>{
-    console.log("Serving at port 3000!");
+const port = process.env.PORT || 3000;
+
+app.listen(port,()=>{
+    console.log(`Serving at port ${port}!`);
 })
