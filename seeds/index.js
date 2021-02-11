bfdcbf691b3a753e72aab72e201d5238f80f36f1
Vartika Rana <vartikarana2000@gmail.com>
2021-02-11 19:50:42 +0530

    seeding

--- seeds/index.js
+++ seeds/index.js
@@ -1,9 +1,14 @@
+if(process.env.NODE_ENV !== "production"){
+    require('dotenv').config();
+}
+
 const mongoose= require('mongoose');
 const cities= require('./INcities');
 const { places, descriptors} = require('./seedHelpers');
 const Campground= require('../models/campground');
 
-mongoose.connect('mongodb://localhost:27017/yelp-camp',{
+const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/yelp-camp';
+mongoose.connect(dbUrl,{
     useNewUrlParser: true,
     useCreateIndex: true,
     useUnifiedTopology: true
